1278e2c108ce47d3526a28fb6b4b55f0e62459a8
Carolyn Hemmingsexit <cphemm@msn.com>
2016-05-27 12:57:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/carolynhemmings/intentexample/SecondActivity.java
@@ -0,0 +1,44 @@
+package com.example.carolynhemmings.intentexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Bundle firstActivityData = getIntent().getExtras();
+        if(firstActivityData==null) {
+            return;
+        }
+        String firstActivityMessage = firstActivityData.getString("firstActivityMessage");
+        final TextView secondActivityText = (TextView) findViewById(R.id.secondActivityText);
+        secondActivityText.setText(firstActivityMessage);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        Intent i = new Intent(this, FirstActivity.class);
+        startActivity(i);
+    }
+
+}
